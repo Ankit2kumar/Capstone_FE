ec4d4bb31b0aacdbf27b6b5d3379ed2b96c3e535
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2021-12-02 13:54:07 +0100

    .

--- src/components/Carousel/ImageCarousel.jsx
+++ src/components/Carousel/ImageCarousel.jsx
@@ -1,33 +1,55 @@
 import './ImageCarousel.css';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Col, Container, Row } from 'react-bootstrap';
 
 const ImageCarousel = () => {
 	return (
-		<div>
-			<Carousel fade>
-				<Carousel.Item>
-					<img
-						className="d-block w-50 h-100"
-						src="/Carousel-Images/golden-temple-image.jpeg?text=First slide&bg=373940"
-						alt="First slide"
-					/>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img
-						className="d-block w-50 h-100"
-						src="/Carousel-Images/qutub-minar-image.jpeg?text=Second slide&bg=282c34"
-						alt="Second slide"
-					/>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img
-						className="d-block w-50 h-100"
-						src="/Carousel-Images/taj-mahal-india-hdr-wallpaper-preview.jpeg?text=Third slide&bg=20232a"
-						alt="Third slide"
-					/>
-				</Carousel.Item>
-			</Carousel>
-		</div>
+		<>
+			<Container fluid className="carousel-container">
+				<Row>
+					<Col xs={12} md={12} lg={6} xl={6}>
+						<Carousel fade>
+							<Carousel.Item>
+								<img
+									className="d-block w-75"
+									src="/Carousel-Images/banner-img.webp?text=First slide&bg=373940"
+									alt="First slide"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									className="d-block w-75"
+									src="/Carousel-Images/banner-img01.webp?text=Second slide&bg=282c34"
+									alt="Second slide"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									className="d-block w-100"
+									src="/Carousel-Images/Chadar-Frozen-River-Trek-Ladakh.jpeg?text=Third slide&bg=20232a"
+									alt="Third slide"
+								/>
+							</Carousel.Item>
+						</Carousel>
+					</Col>
+					<Col xs={12} md={12} lg={6} xl={6}>
+						<div className="mt-3">
+							{' '}
+							<h1>Explore with us your perfect</h1>
+						</div>
+						<div>
+							{' '}
+							<h1>
+								{' '}
+								<strong style={{ color: ' orange' }}>
+									Indian Holiday
+								</strong>{' '}
+							</h1>{' '}
+						</div>
+						<div></div>
+					</Col>
+				</Row>
+			</Container>
+		</>
 	);
 };
 
